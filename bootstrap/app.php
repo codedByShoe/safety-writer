96316291b05ada4e0441f29e1a31e1aa72258e2a
Andrew Shoemaker <andrew.shoemaker@hey.com>
2025-12-14 22:04:03 -0600

    updated trusted proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust Fly.io proxies for HTTPS detection
+        $middleware->trustProxies(at: '*');
+
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state', 'chat_id']);
 
         $middleware->validateCsrfTokens(except: [
